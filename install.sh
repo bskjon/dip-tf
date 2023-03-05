090edb3e2d147d476dd8229a0520bdbd797b0932
Brage Skjønborg <brage.skjonborg@hotmail.no>
2023-03-05 22:28:21 +0100

    Lib based (#1)
    * Re-worked code
    Moved to pypi

--- install.sh
+++ install.sh
@@ -8,25 +8,10 @@ else
     cp /etc/iproute2/rt_tables /etc/iproute2/rt_tables.bak
 fi
 
-chmod +x dipwa.sh
-chmod +x service.py
-
 sudo apt install -y python3-pip
 
 echo "Installing dependencies"
-pip install netaddr -U
-pip install prompt_toolkit==1.0.14
-pip install whaaaaat
-pip install pyroute2
-
-systemctl stop dip-table-flipper.service
-systemctl disable dip-table-flipper.service
-
-
-rm /etc/systemd/system/dip-table-flipper.service
-rm /tmp/dipwa
-rm -r /usr/local/dip-table-flipper
-
+pip install pyDynamicRoutingUpdater -U
 
 systemctl stop dynamic-routing-updater.service
 systemctl disable dynamic-routing-updater.service
@@ -35,18 +20,43 @@ rm /etc/systemd/system/dynamic-routing-updater.service
 
 systemctl daemon-reload
 
-sleep 10s
-
 mkdir --parents /usr/local/dynamic-routing-updater/
-cp ./service.py /usr/local/dynamic-routing-updater/service.py
-cp ./reference.json /usr/local/dynamic-routing-updater/reference.json
-cp ./dipwa.sh /etc/networkd-dispatcher/routable.d/dipwa.sh
-cp ./dipwa.sh /usr/lib/networkd-dispatcher/routable.d/dipwa.sh
 
+echo "Creating DRU Service runner"
+cat > /usr/local/dynamic-routing-updater/service.py <<EOL
+from DynamicRoutingUpdater import DynamicRoutingUpdater
+reference = "reference.json"
+service = DynamicRoutingUpdater()
+service.start()
+EOL
+
+cp ./reference.json /usr/local/dynamic-routing-updater/reference.json
 referenceAbsPath="/usr/local/dynamic-routing-updater/reference.json"
 sed -i "s^reference.json^$referenceAbsPath^g" /usr/local/dynamic-routing-updater/service.py
 
+echo "Creating DIPWA"
+tee > (cat > /etc/networkd-dispatcher/routable.d/dipwa.sh) > (cat > /usr/lib/networkd-dispatcher/routable.d/dipwa.sh)  <<EOL
+#!/bin/sh
 
+# Dynamic Ip Watcher Action (DIPWA)
+# A component of DynamicRoutingUpdater
+# 
+# The purpose of Dipwa is to be notified by the system when there are changes to net network interface
+# If this script is placed correctly inside a hook folder for the network manager, 
+# the network manager will call up this script whith the interface that has been updated or altered
+#
+# This script will then proceed to update a temporary file which the service DRU will watch and respond to
+#
+
+echo "DynamicIpWatcherAction: Registered change to network adpater $IFACE"
+
+if [ ! -z $IFACE ]
+then
+    echo $IFACE > /tmp/dipwa
+fi
+EOL
+
+echo "Creating DRU Service"
 cat > /etc/systemd/system/dynamic-routing-updater.service <<EOL
 [Unit]
 Description=Dynamic Routing Updater - Table flipper
@@ -65,7 +75,12 @@ EOL
 chmod 700 /etc/networkd-dispatcher/routable.d/dipwa.sh
 chmod 700 /usr/lib/networkd-dispatcher/routable.d/dipwa.sh
 
+chmod +x /etc/networkd-dispatcher/routable.d/dipwa.sh
+chmod +x /usr/lib/networkd-dispatcher/routable.d/dipwa.sh
+
+
 chmod 700 /usr/local/dynamic-routing-updater/service.py
+chmod +x /usr/local/dynamic-routing-updater/service.py
 
 chown root:root /usr/local/dynamic-routing-updater/service.py
 
@@ -75,3 +90,5 @@ systemctl enable dynamic-routing-updater.service
 systemctl start dynamic-routing-updater.service
 
 systemctl status dynamic-routing-updater.service
+
+echo "Done!"
