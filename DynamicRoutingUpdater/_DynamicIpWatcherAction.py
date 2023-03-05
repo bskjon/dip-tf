5339010a55f298347228db963181417842d8a355
Brage Skjonborg <brage.skjonborg@hotmail.no>
2023-03-05 20:06:42 +0100

    Changed output

--- DynamicRoutingUpdater/_DynamicIpWatcherAction.py
+++ DynamicRoutingUpdater/_DynamicIpWatcherAction.py
@@ -32,6 +32,13 @@ class _DynamicIpWatcherAction:
         self.nics.extend(nics)
         self.nics_rt = nics_rt
             
+    def stdout(self, out:str):
+        sys.stdout.write(f"{out}\n")
+        sys.stdout.flush()
+    def stderr(self, out:str):
+        sys.stderr.write(f"{out}\n")
+        sys.stderr.flush()
+            
     def start(self) -> None:
         """Starts Thread that opens pipe and watches it for changes
         Returns:
@@ -50,35 +57,35 @@ class _DynamicIpWatcherAction:
         """
         """
         if self.__mainThread == threading.current_thread():
-            sys.stderr.write("DIPWA has not been started in a separete thread!\n")
+            self.stderr("DIPWA has not been started in a separete thread!")
             raise Exception("DIPWA is started in main thread!")
-        sys.stdout.write("DIPWA Thread Started\n")
+        self.stdout("DIPWA Thread Started")
         self.__openPipe()
         
     def __openPipe(self) -> None:
         """_summary_
         """
-        sys.stdout.write(f"Opening pipe on {self.pipe_path}")
+        self.stdout(f"Opening pipe on {self.pipe_path}")
         while not self.stopFlag.is_set():
             with open(self.pipe_path, 'r') as fifo:
                 message = fifo.read().strip("\n")
                 if message and message in self.nics:
-                    sys.stdout.write(f"Recieved valid message: {message}\n")
+                    self.stdout(f"Recieved valid message: {message}")
                     self.__processMessage(message)
                 else:
-                    sys.stderr.write(f"Recieved invalid message: {message}\n")
+                    self.stderr(f"Recieved invalid message: {message}")
             time.sleep(2.5)
     
     def __processMessage(self, nic: str) -> None:
         if (nic not in netifaces.interfaces()):
-            sys.stdout.write(f"Message contains non nic value: {nic}\n")
+            self.stdout(f"Message contains non nic value: {nic}")
             return
-        sys.stdout.write(f"Message indicates that there has been changes to nic: {nic}\n")
+        self.stdout(f"Message indicates that there has been changes to nic: {nic}")
         adapter = NetworkAdapter(nic)
         if (adapter.isValid()):
             self.__routingTable_modify(adapter)
         else:
-            sys.stdout.write(f"Adding puller on {nic}")
+            self.stdout(f"Adding puller on {nic}")
             self.__puller_add(nic)
                 
             
@@ -86,7 +93,7 @@ class _DynamicIpWatcherAction:
         """_summary_
         """
         nic_rt_table = self.nics_rt[adapter.name]
-        sys.stdout.write(f"Modifying routing for {adapter.name} on table {nic_rt_table}")
+        self.stdout(f"Modifying routing for {adapter.name} on table {nic_rt_table}")
         
         self.__routingTable_deleteRoute(adapter=adapter)
         self.__routingTable_deleteRoute(adapter=adapter, tableName=nic_rt_table)
@@ -97,7 +104,7 @@ class _DynamicIpWatcherAction:
     def __routingTable_addRoute(self, adapter: NetworkAdapter, tableName: str) -> None:
         """_summary_
         """
-        sys.stdout.write(f"Adding routes to routing table {tableName}")
+        self.stdout(f"Adding routes to routing table {tableName}")
         if not tableName:
             raise Exception("Routing table name is not preset")
         operations: List[str] = [
@@ -109,14 +116,14 @@ class _DynamicIpWatcherAction:
             #proc = subprocess.run([operation], shell=True, check=True, stdout=subprocess.PIPE)
             result = os.system(operation)
             if result != 0:
-                sys.stderr.write(f"Failed: {operation}\n")
+                self.stderr(f"Failed: {operation}")
             else:
-                sys.stderr.write(f"OK: {operation}\n")
+                self.stderr(f"OK: {operation}")
         
     def __routingTable_addRule(self, adapter: NetworkAdapter, tableName: str) -> None:
         """
         """
-        sys.stdout.write(f"Adding rules to routing table {tableName}")
+        self.stdout(f"Adding rules to routing table {tableName}")
         if not tableName:
             raise Exception("Routing table name is not preset")
         operations: List[str] = [
@@ -126,16 +133,16 @@ class _DynamicIpWatcherAction:
             #proc = subprocess.run([operation], shell=True, check=True, stdout=subprocess.PIPE)
             result = os.system(operation)
             if result != 0:
-                sys.stderr.write(f"Failed: {operation}\n")
+                self.stderr(f"Failed: {operation}")
             else:
-                sys.stderr.write(f"OK: {operation}\n")
+                self.stderr(f"OK: {operation}")
     
     def __routingTable_deleteRoute(self, adapter: NetworkAdapter, tableName: str = "main") -> None:
         """Deletes routes on routing table
             If there is a different ruting table than main, you will need to pass it here
             For removing routes on the default table keep "main" or replace it with the correct one
         """
-        sys.stdout.write(f"Deleting rules on routing table {tableName}")
+        self.stdout(f"Deleting rules on routing table {tableName}")
         if not tableName:
             raise Exception("Routing table name is not preset")
         operations: List[str] = [
@@ -148,9 +155,9 @@ class _DynamicIpWatcherAction:
             #proc = subprocess.run([operation], shell=True, check=True, stdout=subprocess.PIPE)
             result = os.system(operation)
             if result != 0:
-                sys.stderr.write(f"Failed: {operation}\n")
+                self.stderr(f"Failed: {operation}")
             else:
-                sys.stderr.write(f"OK: {operation}\n")
+                self.stderr(f"OK: {operation}")
     
     nicsPullerThreads: List[Thread] = []
 
@@ -159,7 +166,7 @@ class _DynamicIpWatcherAction:
         """
         waitTime: int = 60
         if len(list(filter(lambda x: x.name == nic, self.nicsPullerThreads))) != 0:
-            print(f"Fount existing thread for {nic} skipping..\n")
+            self.stdout(f"Fount existing thread for {nic} skipping..")
             return
         thread = Thread(
             name=nic,
@@ -179,7 +186,7 @@ class _DynamicIpWatcherAction:
     def __puller_thread(self, nic: str, waitTime: int = 60) -> None:
         """Thread for pulling on adapter
         """
-        sys.stdout.write(f"Starting pulling on {nic}\n")
+        self.stdout(f"Starting pulling on {nic}")
         
         isInInvalidState: bool = True
         while isInInvalidState:
@@ -190,6 +197,6 @@ class _DynamicIpWatcherAction:
                 self.__puller_remove(nic)
                 self.__routingTable_modify(adapter)
             else:
-                sys.stdout.write(f"Pulling on {nic} in {waitTime}s")
-        sys.stdout.write(f"Pulling on {nic} has ended")
+                self.stdout(f"Pulling on {nic} in {waitTime}s")
+        self.stdout(f"Pulling on {nic} has ended")
         
